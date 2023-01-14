d4b7d4a61cb7cfcab01f6d10531471c88eef3425
kire21 <kire.zdraveski@gmail.com>
2023-01-14 17:45:48 +0100

    initial commit

---
+++ src/data/navData.js
@@ -0,0 +1,16 @@
+const navData = [
+  {
+    name: "About",
+    path: "/about/",
+  },
+  {
+    name: "Blog",
+    path: "/blog/",
+  },
+  {
+    name: "Draw",
+    path: "/draw/",
+  },
+];
+
+export default navData;
